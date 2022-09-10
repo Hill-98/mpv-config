f6b80926738239cc31c8011f92d0b3fbf97e9942
Zhong Lufan <lufanzhong@gmail.com>
2022-09-11 05:45:48 +0800

    优化 webplay 脚本参数解析

--- scripts/webplay-handler.js
+++ scripts/webplay-handler.js
@@ -1,34 +1,27 @@
 var msg = mp.msg;
-var event = require('../script-modules/EventHelper');
-var LOADED_HANDLER = null;
 var TEMP_URLS = [];
+var PROTOCOL_PREFIX = 'webplay:?';
+var START = null;
 
-function getParam(url, param) {
-    var str = param + '=';
-    var start = url.indexOf(str);
-    if (start === -1) {
-        return null;
+function parse_url(url) {
+    var decode = function (value) {
+        return decodeURIComponent(value.replace(/\+/g, '%20'));
     }
-    start += str.length;
-    var end = url.indexOf('&', start);
-    return decodeURIComponent(url.substring(start, end === -1 ? undefined : end).replace(/\+/g, '%20'));
-}
-
-function loaded(url) {
-    var title = getParam(url, 'title');
-    if (title) {
-        msg.info('Title: ' + title);
-        mp.set_property_native('file-local-options/title', title);
+    var results = {};
+    var params = url.replace(PROTOCOL_PREFIX, '').split('&');
+    for (var i = 0; i < params.length; i++) {
+        var param = params[i].split('=');
+        if (param.length != 2) {
+            continue;
+        }
+        results[param[0]] = decode(param[1]);
     }
-    var start = getParam(url, 'start');
-    if (start && parseFloat(start)) {
-        mp.set_property_native('playback-time', start);
-    }
+    return results;
 }
 
 mp.add_hook('on_load', 40, function () {
     var path = mp.get_property_native('path');
-    if (path.indexOf('webplay:?') !== 0) {
+    if (path.indexOf(PROTOCOL_PREFIX) !== 0) {
         return;
     }
     mp.set_property_native('file-local-options/save-position-on-quit', false);
@@ -38,19 +31,20 @@ mp.add_hook('on_load', 40, function () {
         path = TEMP_URLS[index + 1];
         TEMP_URLS.splice(index, 2);
     }
-    var link = getParam(path, 'link');
-    var parse = getParam(path, 'parse');
-    if (parse === '1') {
-        // 解析链接需要重新执行一次加载文件流程，所以当文件结束事件发生时附加一个新的事件。
-        event.once('end-file', function () {
-            LOADED_HANDLER = loaded.bind(this, path);
-            event.once('file-loaded', LOADED_HANDLER);
-        });
-        mp.command_native(['loadfile', link]);
+    var params = parse_url(path);
+    var link = params.link;
+    var isParse = params.parse === '1';
+    var opts = {};
+    if (params.start !== undefined && parseInt(params.start) > 0) {
+        START = parseInt(params.start);
+        opts.start = START;
+    }
+    if (isParse) {
+        mp.command_native(['loadfile', link, 'replace', ]);
         return;
     }
-    var referer = getParam(path, 'referer');
-    // 使保存的文件设置具有统一性
+    var referer = params.referer;
+    // 保存的文件设置具有统一性
     if (referer !== null && !isTempUrl) {
         var tempUrl = 'webplay:?' + referer;
         TEMP_URLS.push(tempUrl);
@@ -58,15 +52,19 @@ mp.add_hook('on_load', 40, function () {
         mp.command_native(['loadfile', tempUrl]);
         return;
     }
+    var title = params.title || referer || link;
     msg.info('Play: ' + (referer || link));
     mp.set_property_native('stream-open-filename', link);
-    loaded(path);
     mp.set_property_native('file-local-options/save-position-on-quit', true);
+    mp.set_property_native('file-local-options/title', title);
 });
 
-mp.register_event('end-file', function () {
-    if (LOADED_HANDLER !== null) {
-        mp.unregister_event(LOADED_HANDLER);
-        LOADED_HANDLER = null;
+mp.add_hook('on_unload', 50, function () {
+    START = null;
+});
+
+mp.register_event('file-loaded', function () {
+    if (START !== null) {
+        mp.set_property_native('playback-time', START);
     }
 });
