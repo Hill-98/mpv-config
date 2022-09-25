bbac152e6d2c2e8ed4d432ea9948baa5b0e6571b
Zhong Lufan <lufanzhong@gmail.com>
2022-09-26 03:46:50 +0800

    脚本: webplay-handler: 修复检查 referer 参数错误

--- scripts/webplay-handler.js
+++ scripts/webplay-handler.js
@@ -4,32 +4,42 @@ var PROTOCOL_PREFIX = 'webplay:?';
 
 var msg = mp.msg;
 var commands = require('../script-modules/commands');
-/** @type {Array.<string>} */
-var temp_urls = [];
-/** @type {number|null} */
-var temp_start = null;
+var state = {
+    /** @type {number|null} */
+    start: null,
+    /** @type {string} */
+    url: '',
+};
 
 /**
  * @param {string} url
  * @returns {Object.<string, string>}
  */
 function parse_url(url) {
-    var decode = function (value) {
+    var decode = function decode(value) {
         return decodeURIComponent(value.replace(/\+/g, '%20'));
     }
+    var trim_map = function trim_map(value) {
+        return value.trim();
+    }
+    var empty_filter = function empty_filter(value) {
+        return value !== '';
+    }
+
     var results = {};
-    var params = url.replace(PROTOCOL_PREFIX, '').split('&');
+    var params = url.replace(PROTOCOL_PREFIX, '').split('&').map(trim_map).filter(empty_filter);
     for (var i = 0; i < params.length; i++) {
-        var param = params[i].split('=');
-        if (param.length != 2) {
+        var strs = params[i].split('=').map(trim_map).filter(empty_filter);
+        if (strs.length != 2) {
             continue;
         }
-        results[param[0]] = decode(param[1]);
+        results[strs[0]] = decode(strs[1]);
     }
     return results;
 }
 
 mp.add_hook('on_load', 40, function () {
+    state.start = null;
     /** @type {string} */
     var path = mp.get_property_native('path');
     if (path.indexOf(PROTOCOL_PREFIX) !== 0) {
@@ -37,43 +47,41 @@ mp.add_hook('on_load', 40, function () {
     }
     var save_on_quit = mp.get_property_native('save-position-on-quit');
     mp.set_property_native('file-local-options/save-position-on-quit', false);
-    var index = temp_urls.indexOf(path);
-    var isTempUrl = index !== -1;
-    if (isTempUrl) {
-        path = temp_urls[index + 1];
-        temp_urls.splice(index, 2);
+
+    var is_reload = state.url !== '';
+    if (is_reload) {
+        path = state.url;
+        state.url = '';
     }
+
     var params = parse_url(path);
     var link = params.link;
-    var isParse = params.parse === '1';
-    var opts = {};
-    if (params.start !== undefined && parseInt(params.start) > 0) {
-        temp_start = parseInt(params.start);
-        opts.start = temp_start;
-    }
-    if (isParse) {
-        commands.loadfile(link, 'replace');
+    // 重新加载以交给对应解析程序。
+    if (params.parse === '1') {
+        msg.info('WebPlay: ' + link);
+        commands.loadfile(link);
         return;
     }
+
+    // 保存的设置有一致性
     var referer = params.referer;
-    // 保存的文件设置具有统一性
-    if (referer !== null && !isTempUrl) {
-        var tempUrl = 'webplay:?' + referer;
-        temp_urls.push(tempUrl);
-        temp_urls.push(path);
-        commands.loadfile(tempUrl);
+    if (!is_reload && referer) {
+        state.url = path;
+        commands.loadfile('webplay:?' + referer);
         return;
     }
+
     var title = params.title || referer || link;
-    msg.info('Play: ' + (referer || link));
+    var start = parseInt(params.start);
+    state.start = start >= 0 ? start : null;
+    msg.info('WebPlay: ' + (referer || link));
     mp.set_property_native('stream-open-filename', link);
     mp.set_property_native('file-local-options/save-position-on-quit', save_on_quit);
     mp.set_property_native('file-local-options/title', title);
 });
 
 mp.register_event('file-loaded', function () {
-    if (temp_start !== null) {
-        mp.set_property_native('playback-time', temp_start);
-        temp_start = null;
+    if (state.start !== null) {
+        mp.set_property_native('playback-time', state.start);
     }
 });
