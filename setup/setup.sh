8b1ca6fd77bd06c4da458504336fb602e0db482b
Zhong Lufan <lufanzhong@gmail.com>
2022-09-26 17:10:29 +0800

    添加 Linux 配置脚本

---
+++ setup/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cd "$(dirname "$(realpath "$0")")" || exit 1
+cd ..
+
+MPV_XDG_CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/mpv
+MPV_XDG_CONFIG_DIR_BACKUP=$MPV_XDG_CONFIG_DIR.bak.$(date +%Y%m%d%H%M%S)
+
+if [ ! -f local.conf ]; then
+    echo "# 自定义配置文件" > local.conf
+fi
+
+sed "s|%CONFIG_DIR%|$PWD|g" fonts.linux.conf > fonts.conf
+
+cat > mpv.conf << EOF
+include="~~/common.conf"
+include="~~/linux.conf"
+include="~~/local.conf"
+EOF
+
+if [ -d "$MPV_XDG_CONFIG_DIR" ] && [ ! -L "$MPV_XDG_CONFIG_DIR" ]; then
+    echo "Backup '$MPV_XDG_CONFIG_DIR' to '$MPV_XDG_CONFIG_DIR_BACKUP'"
+    mv "$MPV_XDG_CONFIG_DIR" "$MPV_XDG_CONFIG_DIR_BACKUP"
+fi
+
+echo "$MPV_XDG_CONFIG_DIR -> $PWD"
+ln -f -s -T "$PWD" "$MPV_XDG_CONFIG_DIR"
+echo "Done!"
