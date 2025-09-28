ff1ce8baeb0c597fe61c29e7cc7cc1899eb407a9
Zhong Lufan <lufanzhong@gmail.com>
2025-09-28 19:25:50 +0800

    setup: 修复 fix-symbolic-link.ps1 错误

--- setup/fix-symbolic-link.ps1
+++ setup/fix-symbolic-link.ps1
@@ -1,23 +1,24 @@
 using namespace System.IO
 
 [array]$items = @(
-    @{ Link = "scripts\uosc_shared"; Target = "..\git-modules\uosc\scripts\uosc_shared" },
+    @{ Link = "scripts\uosc"; Target = "..\git-modules\uosc\src\uosc" },
     @{ Link = "shaders\ACNet"; Target = "..\git-modules\ACNetGLSL\glsl" }
 )
 [string]$MPV_CONFIG_DIR = [Path]::GetDirectoryName($PSScriptRoot)
 
 foreach ($item in $items) {
     [FileInfo]$link = New-Object -TypeName FileInfo -ArgumentList @([Path]::Combine($MPV_CONFIG_DIR, $item.Link))
-    if ($link.Attributes.HasFlag([FileAttributes]::Directory)) {
-        if ([Directory]::Exists($link.FullName)) {
-            [Directory]::Delete($link.FullName, $true)
-        }
+    [FileInfo]$target = New-Object -TypeName FileInfo -ArgumentList @([Path]::Combine($MPV_CONFIG_DIR, $item.Target))
+    if ([Directory]::Exists($link.FullName)) {
+        [Directory]::Delete($link.FullName, $true)
+    }
+    if ([File]::Exists($link.FullName)) {
+        [File]::Delete($link.FullName)
+    }
+    if ($target.Attributes.HasFlag([FileAttributes]::Directory)) {
         Start-Process -FilePath cmd.exe -ArgumentList @("/c", "mklink", "/D", $link.FullName, $item.Target) -Verb runas -Wait
     }
     else {
-        if ([File]::Exists($link.FullName)) {
-            [File]::Delete($link.FullName)
-        }
         Start-Process -FilePath cmd.exe -ArgumentList @("/c", "mklink", $link.FullName, $item.Target) -Verb runas -Wait
     }
 }
